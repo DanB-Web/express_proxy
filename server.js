9b34348928341dd32ca3251b5617923d8c2c77ba
Dan Bayford <danbeeweb@gmail.com>
2022-08-17 11:11:46 +0100

    remove cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import fs from "fs-extra";
 import path from "path";
 
@@ -8,8 +7,6 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-
-app.use(cors());
 
 // SIMPLE AUTH CHECK
 const auth = (req, res, next) => {
